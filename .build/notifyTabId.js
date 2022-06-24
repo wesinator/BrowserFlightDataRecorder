ddcc56c5d7d1e35a33ac82002d8a904ae0b3a1d0
rheikai <rheikai@protonmail.com>
2022-06-24 12:00:34 +0900

    rm needless dir

--- .build/notifyTabId.js
+++
@@ -1,5 +0,0 @@
-browser.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
-    if (msg.text == "what is my tab_id?") {
-        sendResponse({ tabId: sender.tab.id });
-    }
-});
